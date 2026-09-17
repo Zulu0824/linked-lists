8aa328ab49b287a5022f25e5a500fc65d0aa94cd
Zulu0824 <sriharsha.patta@gmail.com>
2026-09-17 10:09:09 +0530

    Add append(data) method

--- linked-lists.js
+++ linked-lists.js
@@ -9,4 +9,17 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
 }
